1249a39a2e870d93cca29bbc7b910be4075a057e
Carl Turechek <carl@siteraiser.com>
2017-06-15 23:48:44 -0400

    Crypt added

--- app/models/siteadmin/user_model.php
+++ app/models/siteadmin/user_model.php
@@ -44,7 +44,7 @@ public function getUsers(){
 		
 	}	
 	
-public function generateHash($password) {
+	public function generateHash($password) {
 	   	if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
 		        $salt = '$2y$11$' . substr(md5(uniqid(rand(), true)), 0, 22);
 		        return crypt($password, $salt);
